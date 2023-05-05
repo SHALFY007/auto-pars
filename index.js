840cb8df7277ae652b2930b1c1dca1284d2f0fac
Vladislav <vl_evlashin@mail.ru>
2023-05-05 20:59:32 +0300

    Complete pars

--- index.js
+++ index.js
@@ -23,12 +23,63 @@ const URL = "https://auto.ru/volgograd/cars/ford/all/?page=";
 
         while(flag) {
             await page.goto(`${URL}${counter}`)
-            await page.waitForSelector('a.ListingPagination__page')
+            await page.waitForSelector('img.LazyImage__image')
+
+            setTimeout(() => {
+
+            }, 2000)
 
             let html = await page.evaluate(async () => {
+                let page = [];
+
+                try {
+                    let divs = document.querySelectorAll('div.ListingItem__main')
+
+                    divs.forEach(div => {
+                        let obj = {
+                            title: div.querySelector('a.ListingItemTitle__link').innerText !== null ?
+                            div.querySelector('a.ListingItemTitle__link').innerText : 'NO TITLE',
+                            link: div.querySelector('a.ListingItemTitle__link').href !== null ?
+                            div.querySelector('a.ListingItemTitle__link').href : 'NO LINK',
+                            price: div.querySelector('a.ListingItemPriceNew__link-cYuLr').innerText !== null ?
+                            div.querySelector('a.ListingItemPriceNew__link-cYuLr').innerText : 'NO PRICE',
+                            year: div.querySelector('div.ListingItem__year').innerText !== null ?
+                            div.querySelector('div.ListingItem__year').innerText : "NO YEAR",
+                            kmAge: div.querySelector('div.ListingItem__kmAge').innerText !== null ?
+                            div.querySelector('div.ListingItem__kmAge').innerText : "NO kmAge"
+                        }
+
+                        console.log(obj)
+                        page.push(obj)
+                    })
+                    
+                } catch (error) {
+                    console.log(error)
+                }
+
+                return page
 
             }, {waitUntil: 'a.ListingPagination__page'})
+
+            await res.push(html)
+
+            for (let i in res) {
+                console.log(res[i])
+            }
+
+            if (counter === 4) flag = false
+
+            counter++
         }
+
+        res = res.flat()
+
+        fs.writeFile('auto.json', JSON.stringify({'data': res}), err => {
+            if (err) throw err
+
+            console.log("JSON saved")
+            console.log("JSON length- " + res.length) 
+        })
         
     } catch (error) {
         console.log(error)
